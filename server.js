e3951de8c606495cad3af332ea76119d4d790c23
Bryan Jackson <bjack13@gmail.com>
2017-01-12 12:42:48 -0700

    authenticate walkthrough

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const express = require('express');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const session = require('express-session');
+
+const config = require('./config.json');
 
 const app = express();
+app.use(session({
+  secret: config.secret,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(3000, function () {
-  console.log('running on 3000');
+// FacebookStrategy
+
+passport.use('facebook', new FacebookStrategy ({
+  clientId: config.facebook.clientId,
+  clientSecret: config.facebook.clientSecret,
+  callbackUrl:'http://localhost:3000/auth/facebook/callback',
+  profileFields:[]
+},(accessToken,refreashToken, profile, done) =>{
+  // Access the database
+
+  done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  return done(null, user);
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+  res.status(200).send(req.user);
+});
+
+
+
+app.listen(config.port, function () {
+  console.log('running on ' + config.port);
 });
